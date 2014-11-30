f5f6416a8ed36d7c23300668861c8426a47b156c
nekanek <inina.marina@gmail.com>
2014-12-01 00:36:57 +0400

    tasks for java intro done: logParser and Sockets echo Server

---
+++ JavaIntroLogParser/LogParser.java
@@ -0,0 +1,81 @@
+/* 
+ * Takes as input path to file, calculates total time between login and logout for each unique user and outputs it in reversed order.
+ * Assumpitons: 
+ *   - logfile has structure unix_time_stamp, user_id, action (e.g. 123456, 12, login)
+ *   - user always logs out before logging in again
+*/
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class LogParser {
+    
+    private static class User {
+        int id;
+        int timeSpent; // since 1)current unix time is around 1.4mln, 2)10 years ~ 310k of seconds, 3)everyone logs out before logging in again, int should suffice            
+        public User(int id, int timeSpent) {
+            this.id = id;
+            this.timeSpent = timeSpent;
+        }        
+        @Override
+        public String toString() {
+            return id + ",  " + timeSpent;
+        }
+    }
+    
+    private static class timeComparator implements Comparator<User> {
+        @Override
+        public int compare(User a, User b) {
+            return b.timeSpent - a.timeSpent; // reversed order comparator
+        }
+    }    
+    
+    private static HashMap<Integer, User> readLog (String FILE_NAME) throws FileNotFoundException {
+        int id;
+        int time;
+        String action;
+        String LOGIN = "login";
+        HashMap<Integer, User> times = new HashMap<>();
+        Scanner input = new Scanner(new File(FILE_NAME)); 
+        Scanner line;
+        
+        while (input.hasNextLine()) {
+            line = new Scanner(input.nextLine());
+            line.useDelimiter(",\\p{javaWhitespace}");
+            time = line.nextInt();
+            id = line.nextInt();
+            action = line.next();
+            if (action.equals(LOGIN))
+                time *= -1; 
+            if (times.containsKey(id))
+                times.get(id).timeSpent += time;
+            else
+                times.put(id, new User(id, time));
+        }
+        return times; 
+    }
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        try {
+            String FILE_NAME = args[0];
+            int SEC2HOURS = 60*60;
+            HashMap<Integer, User> times = readLog(FILE_NAME);
+            ArrayList<User> timesSorted = new ArrayList<>(times.values());
+            Collections.sort(timesSorted, new timeComparator());
+            System.out.println("Results (id, time spent in seconds, time spent in hours(approx)):");
+            for (User u : timesSorted) 
+                // since we assume that everyone logs out, do not check here for negative total times
+                System.out.println(u + ",  " + (int) u.timeSpent/SEC2HOURS);            
+        }
+        catch (Exception e) {
+            System.out.println("Error: Path to file undetermined.\n" +
+                    "Please, start with path to log file as command-line parameter. E.g.:\n" +
+                    "java LogParser /home/username/log\n");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ JavaSockets/ChatServer.java
@@ -0,0 +1,80 @@
+/* 
+ * Implements simple multithreaded chat server. 
+ * Assumpitons: 
+ *   - no user id's are preserved, no authentication or other means for client to know message author
+ *   - message from one client is delivered to evry connected client (including itself)
+*/
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ChatServer {
+    public static void main(String[] args ) {  
+        try {
+            ArrayList<Socket> connections = new ArrayList<>(1);
+            int i = 0;
+            ServerSocket s = new ServerSocket(8189);
+
+            while (true) {  
+                Socket incoming = s.accept();
+                System.out.println("User #" + i + " connected");
+                connections.add(incoming);
+                Runnable r = new ThreadedEchoHandler(incoming, connections);
+                Thread t = new Thread(r);
+                t.start();
+                i++;
+            }
+        }
+        catch (IOException e) {  
+            e.printStackTrace();
+        }
+    }
+}
+
+class ThreadedEchoHandler implements Runnable {
+    
+    private final Socket incoming;
+    public ArrayList<Socket> connections;
+
+    public ThreadedEchoHandler(Socket s, ArrayList<Socket> c) { 
+        incoming = s;
+        this.connections = c;
+    }
+
+    @Override
+    public void run() {  
+        try {  
+            try {
+                InputStream inStream = incoming.getInputStream();
+                OutputStream outStream = incoming.getOutputStream();
+                Scanner in = new Scanner(inStream);         
+                PrintWriter out = new PrintWriter(outStream, true); // true for autoFlush
+            
+                out.println( "Welcome to chatserver! Enter BYE to exit." );
+            
+                boolean done = false;
+                while (!done && in.hasNextLine()) {  
+                    String line = in.nextLine();
+                    for (Socket s : connections) {
+                        OutputStream sStream = s.getOutputStream();
+                        PrintWriter sOut = new PrintWriter(sStream, true);
+                        sOut.println("> " + line);
+                    }
+                    System.out.println("Echoed (" + line + ") from " + incoming);
+                    if (line.trim().equals("BYE"))
+                        done = true;
+                }
+            }
+            finally {
+                connections.remove(incoming);
+                incoming.close();
+            }
+        }
+        catch (IOException e) {  
+            e.printStackTrace();
+        }
+    }
+}

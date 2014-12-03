0537d4c335fdef62b68a1b64634f818189002a34
nekanek <inina.marina@gmail.com>
2014-12-04 01:38:32 +0400

    function now takes array as argument, sorting is done only if new values were added

--- CLI/task4c.sh
+++ CLI/task4c.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# !!! execute with bash !!!
+# !!! execute with bash ver. >= 4.3 !!!
 
 # draw a graph (hourly, with gnuplot) of 95% quantile of response timesto urls /resume /vacancy /user
 
@@ -12,78 +12,94 @@ date4c="2013-01-18" # date for plotting in 4c
 # ======= 4c =========
 echo "Drawing graph in 4c: "
 
-function hourlyQuant {
-  # compute 95% quantile for previous hour, put into file
-  if (( ${#arrayR[@]} > 0 ));
-  then
-    readarray -t arrayR < <(for a in "${arrayR[@]}"; do echo "$a"; done | sort)
+function hourlyQuant2 () {
+  # uses passing array by refernce (since bash 4.3)
+  declare -n ar=$2
+  if (( ${#ar[@]} > 0));
+  then 
+    # sorting only if during previous hour some values were added
+    if [ "$3" = true ] ;
+    then
+      readarray -t ar < <(for a in "${ar[@]}"; do echo "$a"; done | sort)
+    fi
     # following scary line equals these four:
-      # lengthR=${#arrayR[@]}
+      # lengthR=${#ar[@]}
       # i=$lengthR*95/100
-      # temp=${arrayR[$i]} 
-      # echo $curHour " " $temp >> resumeT
-    echo $curHour " " ${arrayR[$(($((${#arrayR[@]}))*95/100))]}  >> resumeT
-  fi
-  # same for other 2 urls 
-  if ((${#arrayV[@]} > 0));
-  then          
-    readarray -t arrayV < <(for a in "${arrayV[@]}"; do echo "$a"; done | sort)
-    echo $curHour " " ${arrayV[$(($((${#arrayV[@]}))*95/100))]}  >> vacancyT
-  fi
-  if ((${#arrayU[@]} > 0));
-  then
-    readarray -t arrayU < <(for a in "${arrayU[@]}"; do echo "$a"; done | sort)
-    echo $curHour " " ${arrayU[$(($((${#arrayU[@]}))*95/100))]}  >> userT            
-  fi
-      
+      # temp=${ar[$i]} 
+      # echo $curHour " " $temp >> $1
+    echo $curHour " " ${ar[$(($((${#ar[@]}))*95/100))]}  >> $1
+  fi  
 }
 
-# head -1 $in | awk '{gsub(/:*/,""); print $2; } ' $in`
-curHour=`head -1 $in | awk '{print $2;}'`
-curHour=${curHour:0:2}
-
+start=true
+changedR=false
+changedV=false
+changedU=false
+arrayR=()
+arrayV=()
+arrayU=()
 if [ -f $in ]
 then
   while read date ttime log_level request_type user_id url  response_code response_time;
   do
-  # $date $ttime $log_level $request_type $user_id $url  $response_code $response_time
     if [ "$date4c" = "$date" ]
     then
-        done="done"
-        # if current line time is another hour, 
+        done="done" # we found in file date needed
+        # setting first hour
+        if [ "$start" = true ] ; then
+          # if relevant url
+          if [[ "$url" =~ /resume[/?] ]] || [[ "$url" =~ /resume$ ]] || [[ "$url" =~ /vacancy[/?] ]] || [[ "$url" =~ /vacancy$ ]] || [[ "$url" =~ /user[/?] ]] || [[ "$url" =~ /user$ ]] ; then
+              curHour=${ttime:0:2}
+              start=false
+          fi
+        fi
+
+        # after going to next hour, count quantiles for previous
         if (( ${ttime:0:2} > $curHour ));
         then
-          hourlyQuant
+          hourlyQuant2 "resumeT" arrayR "$changedR"
+          hourlyQuant2 "vacancyT" arrayV "$changedV"
+          hourlyQuant2 "userT" arrayU "$changedU"
+          
+          changedR=false
+          changedV=false
+          changedU=false
+
           curHour=$(($curHour + 1))
         fi
-        # add response times to arrays
+
+        # add response times to arrays if relevant
         response_time=`echo $response_time | tr -d "ms"`
         if [[ "$url" =~ /resume[/?] ]] || [[ "$url" =~ /resume$ ]];
         then
-          arrayR=( "${arrayR[@]}" "$response_time" )
+          arrayR+=( "$response_time" )
+          changedR=true
         fi
         if [[ "$url" =~ /vacancy[/?] ]] || [[ "$url" =~ /vacancy$ ]];
         then   
-          arrayV=( "${arrayV[@]}" "$response_time" )
+          arrayV+=( "$response_time" )
+          changedV=true
         fi
         if [[ "$url" =~ /user[/?] ]] || [[ "$url" =~ /user$ ]];
         then
-          arrayU=( "${arrayU[@]}" "$response_time" )
+          arrayU+=( "$response_time" )
+          changedU=true
         fi
     else
+      # skip the rest of file older than required date
       if [ "$done" = "done" ] 
       then
         break
       fi
     fi
   done < $in
-  hourlyQuant
+          hourlyQuant2 "resumeT" arrayR "$changedR"
+          hourlyQuant2 "vacancyT" arrayV "$changedV"
+          hourlyQuant2 "userT" arrayU "$changedU"
 fi
-
 
 gnuplot -p -e "plot 'resumeT' w lines, 'vacancyT' w lines, 'userT' w lines;"
 
 rm resumeT
 rm vacancyT
 rm userT
-
